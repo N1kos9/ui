8d68d36394f27f0ebc641f44935baf2f9f436617
Nikos <onkl3p@gmail.com>
2023-12-23 14:13:36 +0200

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex text-white p-4 pt-5 text-start justify-evenly">
+        <h2 className="">Logo</h2>
+        <div className="">
+          <a href="" className="mr-5">
+            About us
+          </a>
+          <a href="" className="mr-5">
+            Services
+          </a>
+          <a href="" className="mr-5">
+            Plans
+          </a>
+          <a href="" className="mr-5">
+            Blog
+          </a>
+          <a href="" className="mr-5">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
